8560331ea699e645a9e4f51e8f9b9904a8aca8df
David Dobrinskiy <david.dobrinskiy@gmail.com>
2020-04-06 14:35:42 +0300

    export AdjascencySetGraph

--- graph_utils/_nbdev.py
+++ graph_utils/_nbdev.py
@@ -4,9 +4,12 @@ __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"Graph": "00_core.ipynb",
          "AdjacencyMatrixGraph": "00_core.ipynb",
-         "Node": "00_core.ipynb"}
+         "Node": "00_core.ipynb",
+         "AdjacencySetGraph": "00_core.ipynb",
+         "asg": "00_core.ipynb"}
 
-modules = ["core.py"]
+modules = ["core.py",
+           "traversal.py"]
 
 doc_url = "https://ddobrinskiy.github.io/graph_utils/"
 
